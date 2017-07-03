c7ac5384e9a38a7cbde33e2deaa33a685574ce42
Scott Ouellette <scottx611x@gmail.com>
2017-07-03 10:48:08 -0400

    Add cookiecutter docker project code

---
+++ test_runner.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+REPO=scottx611x/refinery-jupyter-docker
+export STAMP=`date +"%Y-%m-%d_%H-%M-%S"`
+
+docker pull $REPO # Defaults to "latest", but just speeds up the build, so precise version doesn't matter.
+docker build --cache-from $REPO \
+             --tag image-$STAMP \
+             .
+
+# Keep this simple: We want folks just to be able to run the bare Docker container.
+# If this starts to get sufficiently complicated that we want to put it in a script
+# by itself, then it has gotten too complicated.
+export SUFFIX=-standalone
+echo "image-$STAMP"
+
+CONTAINER_NAME="container-$STAMP$SUFFIX"
+
+mkdir "/tmp/$CONTAINER_NAME"
+
+docker run --name $CONTAINER_NAME \
+           --detach \
+           --publish-all \
+           --volume /tmp/$CONTAINER_NAME:/refinery-data \
+           image-$STAMP
+
+python tests.py
+
+rm -r "/tmp/$CONTAINER_NAME"
---
+++ tests.py
@@ -0,0 +1,49 @@
+import json
+import os
+import requests
+import subprocess
+import time
+import unittest
+
+
+class CommandlineTest(unittest.TestCase):
+
+    def setUp(self):
+        command = "docker port container-{STAMP}{SUFFIX} | perl -pne 's/.*://'".format(
+            **os.environ)
+        os.environ['PORT'] = subprocess.check_output(
+            command, shell=True).strip().decode('utf-8')
+        self.url = 'http://localhost:{PORT}/api/v1/tilesets/'.format(
+            **os.environ)
+        while True:
+            if 0 == subprocess.call('curl --fail --silent ' + self.url + ' > /dev/null', shell=True):
+                break
+            print('still waiting for server...')
+            time.sleep(1)
+
+    def assertRun(self, command, res=[r'']):
+        output = subprocess.check_output(
+            command.format(**os.environ), shell=True).strip()
+        for re in res:
+            self.assertRegexpMatches(output, re)
+
+    # Tests:
+    def test_hello(self):
+        self.assertRun('echo "hello?"', [r'hello'])
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(CommandlineTest)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    lines = [
+        'browse:  http://localhost:{PORT}/',
+        'shell:   docker exec --interactive --tty container-{STAMP}{SUFFIX} bash',
+        'logs:    docker exec container-{STAMP}{SUFFIX} ./logs.sh'
+    ]
+    for line in lines:
+        print(line.format(**os.environ))
+    if result.wasSuccessful():
+        print('PASS!')
+    else:
+        print('FAIL!')
+        exit(1)
